66696fa40c10445eb72b3808e8326a7e7a0e42fe
Patrick Law <plj611@outlook.com>
2019-02-26 18:36:07 +0800

    Testing function based view

--- indicator/views.py
+++ indicator/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from rest_framework import generics
+#from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
+#from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import LighthouseTest
 from .serializers import CmdSerializer
 
@@ -13,6 +17,7 @@ class ListCmdView(generics.ListAPIView):
    serializer_class = CmdSerializer
 '''
 
+'''
 def cmd_list(request):
    if request.method == 'GET':
       queryset = LighthouseTest.objects.all()
@@ -29,3 +34,22 @@ def cmd_detail(request, cmd_id):
    if request.method == 'GET':
       serializer = CmdSerializer(res)
       return JsonResponse(serializer.data, safe=False)
+'''
+
+@api_view(['GET'])
+def cmd_list(request):
+   if request.method == 'GET':
+      queryset = LighthouseTest.objects.all()
+      serializer = CmdSerializer(queryset, many=True)
+      return Response(serializer.data)
+
+@api_view(['GET'])
+def cmd_detail(request, cmd_id):
+   if request.method == 'GET':
+      try:
+         res = LighthouseTest.objects.get(cmd=cmd_id)
+      except:
+         return Response(status=status.HTTP_404_NOT_FOUND)
+
+      serializer = CmdSerializer(res)
+      return Response(serializer.data)
