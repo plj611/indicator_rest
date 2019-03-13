12dc00eb5c3c7400eb0a4b45253559f47c87445b
Patrick Law <plj611@outlook.com>
2019-03-13 16:43:34 +0800

    Add create_date_time and visit_date_time to model and serializer

--- indicator/models.py
+++ indicator/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Lighthouse(models.Model):
    cmd = models.CharField(max_length=2, null=True)
    act = models.CharField(max_length=1, null=True)
-   date_time = models.DateTimeField(null=True)
+   create_date_time = models.DateTimeField(null=True)
+   visit_date_time = models.DateTimeField(null=True)
    visit_ip = models.CharField(max_length=15, null=True)
 
--- indicator/serializers.py
+++ indicator/serializers.py
@@ -6,4 +6,4 @@ class CmdSerializer(serializers.ModelSerializer):
 #class CmdSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Lighthouse
-        fields = ("cmd", "act", "date_time", "visit_ip")
+        fields = ("cmd", "act", "create_date_time", "visit_date_time", "visit_ip")
